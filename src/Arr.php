a26b642653fa76a5a4f2fa95da3c01fcfc87da9a
Anton Dukhanin <anton.dukhanin@gmail.com>
2016-08-31 18:13:37 +0500

    fix

--- src/Arr.php
+++ src/Arr.php
@@ -6,25 +6,43 @@ use Illuminate\Support\Arr as BaseArr;
 class Arr extends BaseArr
 {
 
-    public static function before($array, $key, $value, $keyBefore = null)
+    public static function before(&$array, $key, $value, $keyBefore = null)
     {
         return static::beforeOrAfter($array, $key, $value, $keyBefore);
     }
 
 
-    public static function after($array, $key, $value, $keyAfter = null)
+    public static function after(&$array, $key, $value, $keyAfter = null)
     {
         return static::beforeOrAfter($array, $key, $value, $keyAfter, true);
     }
 
 
-    protected static function beforeOrAfter($array, $key, $value, $keyNeighbor = null, $after = false)
+    protected static function beforeOrAfter(&$array, $key, $value, $keyNeighbor = null, $after = false)
+    {
+        if (isset( $array[$key] )) {
+            unset( $array[$key] );
+        }
+
+        if (is_null($keyNeighbor) || ! is_array($array) || ! array_key_exists($keyNeighbor, $array)) {
+            return $after ? static::add($array, $value, $key) : static::prepend($array, $value, $key);
+        }
+
+        $keyNeighborIndex = array_search($keyNeighbor, array_keys($array));
+        $sliceLength      = $keyNeighborIndex + ( $after ? 1 : 0 );
+        $left             = array_slice($array, 0, $sliceLength);
+        $right            = array_slice($array, $sliceLength);
+
+        return $array = $left + [ $key => $value ] + $right;
+    }
+
+
+    protected static function beforeOrAfterDot(&$array, $key, $value, $keyNeighbor = null, $after = false)
     {
         if (is_null($keyNeighbor)) {
             return $after ? static::set($array, $value, $key) : static::prepend($array, $value, $key);
         }
 
-        array_set($valueArray, $key, $value);
         $segments        = explode('.', $keyNeighbor);
         $keyNeighborLast = array_pop($segments);
 
@@ -50,7 +68,7 @@ class Arr extends BaseArr
             $left  = array_slice($leveledArray, 0, $keyNeighborIndex + ( $after ? 1 : 0 ));
             $right = array_slice($leveledArray, $keyNeighborIndex);
 
-            $leveledArray = $left + $valueArray + $right;
+            $leveledArray = $left + [ $key => $value ] + $right;
         } else {
             array_set($leveledArray, $key, $value);
         }
