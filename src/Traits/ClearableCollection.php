12a2ebe3dfdc7541962b79ad8c62106f4d90a889
Anton Dukhanin <anton.dukhanin@gmail.com>
2017-03-20 08:13:23 +0500

    ClearableCollection

---
+++ src/Traits/ClearableCollection.php
@@ -0,0 +1,15 @@
+<?php
+namespace Dukhanin\Support\Traits;
+
+trait ClearableCollection
+{
+
+    public function clear()
+    {
+        foreach (array_keys($this->items) as $key) {
+            $this->offsetUnset($key);
+        }
+
+        return $this;
+    }
+}
